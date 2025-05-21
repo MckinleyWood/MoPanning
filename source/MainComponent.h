94fa68422332ab191607be1520eaa20fab8b47f7
MckinleyWood <mwood@dhdev.ca>
2025-05-21 15:19:51 -0700

    more setup

--- source/MainComponent.h
+++ source/MainComponent.h
@@ -1,15 +1,17 @@
 #pragma once
 
-// CMake builds don't use an AppConfig.h, so it's safe to include juce module headers
-// directly. If you need to remain compatible with Projucer-generated builds, and
-// have called `juce_generate_juce_header(<thisTarget>)` in your CMakeLists.txt,
-// you could `#include <JuceHeader.h>` here instead, to make all your module headers visible.
+// CMake builds don't use an AppConfig.h, so it's safe to include juce 
+// module headers directly. If you need to remain compatible with 
+// Projucer-generated builds, and have called 
+// `juce_generate_juce_header(<thisTarget>)` in your CMakeLists.txt, you
+// could `#include <JuceHeader.h>` here instead, to make all your module 
+// headers visible.
 #include <JuceHeader.h>
 
 //==============================================================================
 /*
-    This component lives inside our window, and this is where you should put all
-    your controls and content.
+    This component lives inside our window, and this is where you should 
+    put all your controls and content.
 */
 class MainComponent final : public juce::Component
 {
