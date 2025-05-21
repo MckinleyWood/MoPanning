4f4289c27f5f54835eb47ba3d84aad3d6780b040
MckinleyWood <mwood@dhdev.ca>
2025-05-21 16:03:36 -0700

    beutification and comprehension

--- source/Main.cpp
+++ source/Main.cpp
@@ -1,56 +1,65 @@
 #include "MainComponent.h"
+#include <JuceHeader.h>
 
-//==============================================================================
+//=============================================================================
 class GuiAppApplication final : public juce::JUCEApplication
 {
 public:
-    //==============================================================================
+    //=========================================================================
     GuiAppApplication() {}
 
-    // We inject these as compile definitions from the CMakeLists.txt
-    // If you've enabled the juce header with `juce_generate_juce_header(<thisTarget>)`
-    // you could `#include <JuceHeader.h>` and use `ProjectInfo::projectName` etc. instead.
-    const juce::String getApplicationName() override       { return JUCE_APPLICATION_NAME_STRING; }
-    const juce::String getApplicationVersion() override    { return JUCE_APPLICATION_VERSION_STRING; }
-    bool moreThanOneInstanceAllowed() override             { return true; }
+    const juce::String getApplicationName() override 
+    { 
+        return ProjectInfo::projectName; 
+    }
 
-    //==============================================================================
+    const juce::String getApplicationVersion() override
+    { 
+        return ProjectInfo::versionString; 
+    }
+
+    bool moreThanOneInstanceAllowed() override
+    { 
+        return true; 
+    }
+
+    //=========================================================================
     void initialise (const juce::String& commandLine) override
     {
-        // This method is where you should put your application's initialisation code..
+        // App initialization code goes here.
         juce::ignoreUnused (commandLine);
 
         mainWindow.reset (new MainWindow (getApplicationName()));
     }
 
+
     void shutdown() override
     {
-        // Add your application's shutdown code here..
+        // App shutdown code goes here.
 
         mainWindow = nullptr; // (deletes our window)
     }
 
-    //==============================================================================
+    //=========================================================================
+    /* Called when the app is being asked to quit. */
     void systemRequestedQuit() override
     {
-        // This is called when the app is being asked to quit: you can ignore this
-        // request and let the app carry on running, or call quit() to allow the app to close.
         quit();
     }
 
+
+    /* When another instance of the app is launched while this one is 
+    running, this method is invoked, and the commandLine parameter tells
+    you what the other instance's command-line arguments were. */
     void anotherInstanceStarted (const juce::String& commandLine) override
     {
-        // When another instance of the app is launched while this one is running,
-        // this method is invoked, and the commandLine parameter tells you what
-        // the other instance's command-line arguments were.
         juce::ignoreUnused (commandLine);
     }
 
-    //==============================================================================
-    /*
-        This class implements the desktop window that contains an instance of
-        our MainComponent class.
-    */
+
+    //=========================================================================
+    /* This class implements the desktop window that contains an 
+    instance of our MainComponent class. */
     class MainWindow final : public juce::DocumentWindow
     {
     public:
@@ -73,20 +82,18 @@ public:
             setVisible (true);
         }
 
+        /* This is called when the user tries to close this window. */
         void closeButtonPressed() override
         {
-            // This is called when the user tries to close this window. Here, we'll just
-            // ask the app to quit when this happens, but you can change this to do
-            // whatever you need.
             getInstance()->systemRequestedQuit();
         }
 
-        /* Note: Be careful if you override any DocumentWindow methods - the base
-           class uses a lot of them, so by overriding you might break its functionality.
-           It's best to do all your work in your content component instead, but if
-           you really have to override any DocumentWindow methods, make sure your
-           subclass also calls the superclass's method.
-        */
+        /* Note: Be careful if you override any DocumentWindow methods 
+        - the base class uses a lot of them, so by overriding you might 
+        break its functionality. It's best to do all your work in your 
+        content component instead, but if you really have to override 
+        any DocumentWindow methods, make sure your subclass also calls 
+        the superclass's method. */
 
     private:
         JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainWindow)
@@ -96,6 +103,6 @@ private:
     std::unique_ptr<MainWindow> mainWindow;
 };
 
-//==============================================================================
+//=============================================================================
 // This macro generates the main() routine that launches the app.
 START_JUCE_APPLICATION (GuiAppApplication)
